12f8680843054228392f073a9300018c18179a94
Amy Rager <amy.c.rager@gmail.com>
2022-01-22 21:15:24 -0500

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Holy Moly, DevOps Tutorial! And now it"s automated'));
+app.get('/', (req, res) => res.send('Hello World! Holy Moly, DevOps Tutorial! And now it is automated'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
